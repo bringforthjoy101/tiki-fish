3833e1357a54f38af68d79034e346e4d03911a11
Emmanuel Adelugba <adelugba.emma@gmail.com>
2024-09-14 19:26:49 +0100

    fix: reports pagination

--- src/views/dashboard/analytics/index.js
+++ src/views/dashboard/analytics/index.js
@@ -43,9 +43,9 @@ const AnalyticsDashboard = () => {
 
 	const numFormatter = (num) => {
 		if (num > 999 && num < 1000000) {
-			return `${(num / 1000).toFixed(1)}K`
+			return `${(num / 1000).toFixed(2)}K`
 		} else if (num > 1000000) {
-			return `${(num / 1000000).toFixed(1)}M`
+			return `${(num / 1000000).toFixed(2)}M`
 		} else if (num < 900) {
 			return num
 		}
